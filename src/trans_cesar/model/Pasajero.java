35713f1bc1a5f1dd60bc292423be621ced30a163
AndreaCarolina <acarolinaflorez@unicesar.edu.co>
2026-03-10 14:39:30 -0500

    Crear clase Ticket

--- src/trans_cesar/model/Pasajero.java
+++ src/trans_cesar/model/Pasajero.java
@@ -27,5 +27,4 @@ public class Pasajero extends Persona {
     public void setDescuento(String descuento) {
         this.descuento = descuento;
     }
-
 }
---
+++ src/trans_cesar/model/Ticket.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trans_cesar.model;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author ANDREA CAROLINA
+ */
+public class Ticket {
+    private int NumeroTicket;
+    private LocalDate fechaCompra;
+    private String OrigenCiudad;
+    private String DestinoCiudad;
+    private double PrecioFinal;
+    private Pasajero pasajero;
+    private Vehiculo vehiculo;
+
+    public Ticket(int NumeroTicket, LocalDate fechaCompra, String OrigenCiudad, String DestinoCiudad, double PrecioFinal, Pasajero pasajero, Vehiculo vehiculo) {
+        this.NumeroTicket = NumeroTicket;
+        this.fechaCompra = fechaCompra;
+        this.OrigenCiudad = OrigenCiudad;
+        this.DestinoCiudad = DestinoCiudad;
+        this.PrecioFinal = PrecioFinal;
+        this.pasajero = pasajero;
+        this.vehiculo = vehiculo;
+    }
+
+    public int getNumeroTicket() {
+        return NumeroTicket;
+    }
+
+    public void setNumeroTicket(int NumeroTicket) {
+        this.NumeroTicket = NumeroTicket;
+    }
+
+    public LocalDate getFechaCompra() {
+        return fechaCompra;
+    }
+
+    public void setFechaCompra(LocalDate fechaCompra) {
+        this.fechaCompra = fechaCompra;
+    }
+
+    public String getOrigenCiudad() {
+        return OrigenCiudad;
+    }
+
+    public void setOrigenCiudad(String OrigenCiudad) {
+        this.OrigenCiudad = OrigenCiudad;
+    }
+
+    public String getDestinoCiudad() {
+        return DestinoCiudad;
+    }
+
+    public void setDestinoCiudad(String DestinoCiudad) {
+        this.DestinoCiudad = DestinoCiudad;
+    }
+
+    public double getPrecioFinal() {
+        return PrecioFinal;
+    }
+
+    public void setPrecioFinal(double PrecioFinal) {
+        this.PrecioFinal = PrecioFinal;
+    }
+
+    public Pasajero getPasajero() {
+        return pasajero;
+    }
+
+    public void setPasajero(Pasajero pasajero) {
+        this.pasajero = pasajero;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public void setVehiculo(Vehiculo vehiculo) {
+        this.vehiculo = vehiculo;
+    }
+    
+}
