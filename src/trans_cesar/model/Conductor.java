e1fc5592b9a6d2c59aaf94548774fde07c396b16
AndreaCarolina <acarolinaflorez@unicesar.edu.co>
2026-03-10 14:09:01 -0500

    Crear Clase Persona

---
+++ src/trans_cesar/model/Conductor.java
@@ -0,0 +1,12 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trans_cesar.model;
+
+/**
+ *
+ * @author ANDREA CAROLINA
+ */
+public class Conductor {
+}
