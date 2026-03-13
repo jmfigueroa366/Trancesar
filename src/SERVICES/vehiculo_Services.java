816ff3fea8a2f0c88cde9d705d20fd0cdcdc3ee9
aacadavid <aacadavid@unicesar.edu.co>
2026-03-13 09:00:09 -0500

    feat: creacion de metodo de validacion de capacidad

--- src/SERVICES/vehiculo_Services.java
+++ src/SERVICES/vehiculo_Services.java
@@ -4,6 +4,11 @@
  */
 package SERVICES;
 
+import MODEL.Vehiculo;
+import MODEL.Bus;
+import MODEL.Buseta;
+import MODEL.MicroBus;
+
 /**
  *
  * @author alvar
@@ -12,7 +17,19 @@ public class vehiculo_Services {
 
     public void capacidad(int capacidad) {
         
+        Vehiculo v = null;
         
+        if (v instanceof Bus && v.getCapacidad()>45) {
+            System.out.println("El bus no puede tener mas de 45 pasajeros");
+        }
+        
+        if (v instanceof Buseta && v.getCapacidad()>19) {
+            System.out.println("La buseta no puede tener mas de 19 pasajeros");
+        }
+        
+        if (v instanceof MicroBus && v.getCapacidad()>25) {
+            System.out.println("El microBus no puede tener una capacidad mayor a 25 pasajeros");
+        }
         
     }
 
