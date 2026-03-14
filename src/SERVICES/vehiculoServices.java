9b14950b1a1edaa59556c919c061903c9b4b197e
aacadavid <aacadavid@unicesar.edu.co>
2026-03-14 10:36:27 -0500

    feat: correcion de validacion de tarifa y capacidad

--- src/SERVICES/vehiculoServices.java
+++ src/SERVICES/vehiculoServices.java
@@ -21,31 +21,31 @@ public class vehiculoServices {
     public void validarCapacidad(Vehiculo v)throws Exception {
         
         if (v instanceof Bus && v.getCapacidad()>45) {
-            System.out.println("El bus no puede tener mas de 45 pasajeros");
+            throw new Exception("El bus no puede tener mas de 45 pasajeros");
         }
         
         if (v instanceof Buseta && v.getCapacidad()>19) {
-            System.out.println("La buseta no puede tener mas de 19 pasajeros");
+            throw new Exception("La buseta no puede tener mas de 19 pasajeros");
         }
         
         if (v instanceof MicroBus && v.getCapacidad()>25) {
-            System.out.println("El microBus no puede tener una capacidad mayor a 25 pasajeros");
+           throw new Exception("El microBus no puede tener una capacidad mayor a 25 pasajeros");
         }
         
     }
     
     public void validarTarifa(Vehiculo v)throws Exception{
         
-        if (v instanceof Bus && v.getTarifa()<15000) {
-            System.out.println("El bus no puede tener mas de 45 pasajeros");
+        if (v instanceof Bus && v.getTarifa() < 15000) {
+        throw new Exception("La tarifa del Bus no puede ser menor a $15.000");
         }
-        
-        if (v instanceof Buseta && v.getTarifa()<8000) {
-            System.out.println("La buseta no puede tener mas de 19 pasajeros");
+    
+        if (v instanceof Buseta && v.getTarifa() < 8000) {
+            throw new Exception("La tarifa de la Buseta no puede ser menor a $8.000");
         }
-        
-        if (v instanceof MicroBus && v.getTarifa()<10000) {
-            System.out.println("El microBus no puede tener una capacidad mayor a 25 pasajeros");
+
+        if (v instanceof MicroBus && v.getTarifa() < 10000) {
+            throw new Exception("La tarifa del MicroBus no puede ser menor a $10.000");
         }
         
     }
