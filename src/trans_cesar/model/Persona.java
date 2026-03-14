ca97d252d19cb3dc9ceab6bca4ee4c50f7dfe171
AndreaCarolina <acarolinaflorez@unicesar.edu.co>
2026-03-14 00:27:07 -0500

    Actualización de clase Persona

--- src/trans_cesar/model/Persona.java
+++ src/trans_cesar/model/Persona.java
@@ -3,23 +3,21 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package trans_cesar.model;
+import java.time.LocalDate;
 
 /**
  *
  * @author ANDREA CAROLINA
  */
-public abstract class Persona {
+public abstract class Persona implements Imprimible {
     private int id;
     private String nombre;
-    private int edad;
+    private LocalDate fechaNacimiento;
 
-    public Persona() {
-    }
-
-    public Persona(int id, String nombre, int edad) {
+    public Persona(int id, String nombre, LocalDate fechaNacimiento) {
         this.id = id;
         this.nombre = nombre;
-        this.edad = edad;
+        this.fechaNacimiento = fechaNacimiento;
     }
 
     public int getId() {
@@ -38,11 +36,16 @@ public abstract class Persona {
         this.nombre = nombre;
     }
 
-    public int getEdad() {
-        return edad;
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
     }
 
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }  
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    @Override
+    public void imprimirDetalle() {
+        
+    }
 }
