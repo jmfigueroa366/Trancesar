a9bd0cbff539d6de8da7ad80e7e7f69e4c5faf1d
aacadavid <aacadavid@unicesar.edu.co>
2026-03-19 10:51:39 -0500

    feat: metodo de guardado de rutas en el archivo plano

--- src/DAO/RutaDAO.java
+++ src/DAO/RutaDAO.java
@@ -4,12 +4,30 @@
  */
 package DAO;
 
+import MODEL.Ruta;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import trancesar.util.RutaArchivos;
+
 /**
  *
  * @author alvar
  */
 public class RutaDAO {
 
-    public RutaDAO() {
+    public void guardar(Ruta r) {
+        try (BufferedWriter bw = new BufferedWriter(
+                new FileWriter(RutaArchivos.rutas, true))) {
+            bw.write(r.getCodigo() + ";" +
+                     r.getC_origen() + ";" +
+                     r.getC_destino() + ";" +
+                     r.getDistancia() + ";" +
+                     r.getTiempo());
+            bw.newLine();
+        } catch (IOException e) {
+            System.out.println("Error al guardar ruta: " + e.getMessage());
+        }
     }
+    
 }
