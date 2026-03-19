ddb2670dfedb9eac3016bf24f9b0640c6d2421ce
aacadavid <aacadavid@unicesar.edu.co>
2026-03-19 14:29:46 -0500

    feat: creacion de modelo base del menu de rutas

--- src/VIEW/menuRutas.java
+++ src/VIEW/menuRutas.java
@@ -4,10 +4,58 @@
  */
 package VIEW;
 
+import SERVICES.rutaServices;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  *
  * @author alvar
  */
 public class menuRutas {
+
+    public menuRutas() throws IOException {
+        BufferedReader leer = new BufferedReader(new InputStreamReader(System.in));
+        rutaServices rs = new rutaServices();
+        
+        int opc=0;
+        
+        while (opc!=4) {            
+            
+            System.out.println("==============================");
+            System.out.println("|       GESTIÓN DE RUTAS             |");
+            System.out.println("==============================");
+            System.out.println("|  1. Registrar ruta                 |");
+            System.out.println("|  2. Buscar ruta                    |");
+            System.out.println("|  3. Listar rutas                   |");
+            System.out.println("|  4. Salir                          |");
+            System.out.println("==============================");
+            System.out.print("   Seleccione una opción: ");
+            
+            try {
+                
+                opc=Integer.parseInt(leer.readLine());
+                
+            } catch (NumberFormatException e) {
+                
+                System.out.println("Ingrese solo números");
+                opc = 0;
+                
+            }
+            
+            switch (opc) {
+                case 1:
+                    
+                    break;
+                default:
+                    System.out.println("INGRESE UNA OPCION DEL MENÚ");
+            }
+            
+        }
+        
+    }
+    
+      
     
 }
