249a1770bfc65fe4a0774d79f70ae3108a1af935
jesus figueroa <jmfigueroa@unicesar.edu.co>
2026-03-21 09:34:22 -0500

    fix:corregir clase principal y ejecusion del sistema

---
+++ src/view/MenuPrincipal.java
@@ -0,0 +1,38 @@
+
+package view;
+
+import java.util.Scanner;
+
+public class MenuPrincipal {
+    Scanner sc = new Scanner(System.in);
+
+    public void mostrarMenu() {
+        int opcion;
+
+        do {
+            System.out.println("\n=== SISTEMA TRANSCESAR ===");
+            System.out.println("1. Registrar Vehículo");
+            System.out.println("2. Registrar Pasajero");
+            System.out.println("3. Vender Ticket");
+            System.out.println("4. Ver Reportes");
+            System.out.println("0. Salir");
+            opcion = sc.nextInt();
+
+            switch (opcion) {
+                case 1:
+                    registrarVehiculo();
+                    break;
+                case 2:
+                    registrarPasajero();
+                    break;
+                case 3:
+                    venderTicket();
+                    break;
+                case 4:
+                    mostrarReportes();
+                    break;
+            }
+
+        } while (opcion != 0);
+}
+}
---
+++ src/view/Trans_cesar.java
@@ -0,0 +1,10 @@
+package view;
+
+public class Trans_cesar {
+
+    public static void main(String[] args) {
+        MenuPrincipal m = new MenuPrincipal();
+        m.mostrarMenu();
+    }
+    
+}
