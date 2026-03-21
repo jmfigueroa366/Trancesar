52b8cdc349d0b070e2cd2d77d8e5e6a6eb56ae3e
aacadavid <aacadavid@unicesar.edu.co>
2026-03-21 09:07:54 -0500

    feat: creacion de atributos, getter y setter de la clase reserva

--- src/MODEL/Reservas.java
+++ src/MODEL/Reservas.java
@@ -10,5 +10,46 @@ package MODEL;
  */
 public class Reservas {
     
-    
+    private String codigo;
+    private Pasajero pasajero;
+    private Vehiculo vehiculo;
+    private String fecha_creacion;
+    private String fecha_reserva;
+    private boolean activo;
+
+    public Reservas() {
+    }
+
+    public Reservas(String codigo, Pasajero pasajero, Vehiculo vehiculo, String fecha_creacion, String fecha_reserva, boolean activo) {
+        this.codigo = codigo;
+        this.pasajero = pasajero;
+        this.vehiculo = vehiculo;
+        this.fecha_creacion = fecha_creacion;
+        this.fecha_reserva = fecha_reserva;
+        this.activo = activo;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public Pasajero getPasajero() {
+        return pasajero;
+    }
+
+    public Vehiculo getVehiculo() {
+        return vehiculo;
+    }
+
+    public String getFecha_creacion() {
+        return fecha_creacion;
+    }
+
+    public String getFecha_reserva() {
+        return fecha_reserva;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
 }
