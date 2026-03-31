99d1e741870f38833e20c7ec2ca9be979e887ed3
aacadavid <aacadavid@unicesar.edu.co>
2026-03-31 17:00:00 -0500

    feat: imprimible en la clase microbus

--- src/MODEL/microBus.java
+++ src/MODEL/microBus.java
@@ -20,5 +20,13 @@ public class MicroBus extends Vehiculo{
     @Override
     public void imprimirDetalle() {
         
+        String disponibilidad = isDisponible() ? "DISPONIBLE" : "OCUPADO";
+        
+        System.out.println("MICROBUS");
+        System.out.println("CAPACIDAD: " + "45 PASAJEROS");
+        System.out.println("TARIFA: " + getTarifa());
+        System.out.println("PLACA: " + getPlaca());
+        System.out.println("RUTA: " + getRuta());
+        System.out.println("ESTADO " + disponibilidad);
     }
 }
