43205f40ded4111bbe326cf5d9cc92979a239f7e
jesus figueroa <jmfigueroa@unicesar.edu.co>
2026-04-06 22:49:48 -0500

    subiendo carpeta trancesar

--- src/SERVICES/ConductorService.java
+++ src/SERVICES/ConductorService.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package SERVICES;
 import DAO.ConductorDAO;
 import MODEL.Conductor;
